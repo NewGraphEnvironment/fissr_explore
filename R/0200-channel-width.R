37168cf00f5ad8ba1aeb77da5ff24ddccad88496
NewGraphEnvironment <al@newgraphenvironment.com>
2022-01-18 07:34:36 -0800

    add modelled channel width outputs to a csv

---
+++ R/0200-channel-width.R
@@ -0,0 +1,56 @@
+
+##Get output of channel width equations with R so we can conmapre with sql
+library(tidyverse)
+library(data.table)
+
+
+fiss_density_pts <- read_csv("data/fiss_density_pts.csv",
+                             col_types =cols(
+                               fiss_density_distinct_id = col_double(),
+                               fiss_density_ids = col_character(),
+                               distance_to_stream = col_double(),
+                               stream_order = col_double(),
+                               stream_order_parent = col_double(),
+                               stream_magnitude = col_double(),
+                               gradient = col_double(),
+                               map = col_double(),
+                               map_upstream = col_double(),
+                               upstream_area_ha = col_double(),
+                               channel_width = col_double(),
+                               channel_width_source = col_character(),
+                               mad_m3s = col_double(),
+                               bcalbers_x = col_double(),
+                               bcalbers_y = col_double()
+                             ))
+
+##define parameters from
+# channel-width-21 https://www.poissonconsulting.ca/temporary-hidden-link/1792764180/channel-width-21/
+# channel-width-21b https://www.poissonconsulting.ca/temporary-hidden-link/859859031/channel-width-21b/
+b0_21 <- -2.2383120
+bArea_21	<- 0.3121556
+bPrecipitation_21 <- 0.6546995
+b0_21b <- 0.3071300
+bDischarge_21b <- 0.4577882
+
+fiss_density_pts_cw <- fiss_density_pts %>%
+  mutate(
+    upstream_area_km = upstream_area_ha/100,
+    map_upstream_m = map_upstream/1000,
+    map_upstream_2021 = map_upstream/10,  ##not sure why this is only divided by 10.... Should look at poisson code
+    cw_modelled_21 = exp(b0_21 + (bArea_21 * log(upstream_area_km))  + (bPrecipitation_21 * log(map_upstream_2021))),
+    cw_modelled_21b = exp(b0_21b + bDischarge_21b * (log(upstream_area_km) + log(map_upstream_m)))
+  )
+
+fiss_density_pts_cw_compare <- fiss_density_pts_cw
+    dplyr::relocate(c(channel_width, channel_width_source), .after = cw_modelled_21b)) %>%
+  filter(channel_width_source %ilike% 'Field')
+
+##burn at csv of the values so we can compare to sql
+fiss_density_pts_cw %>%
+  readr::write_csv(file = 'data/fiss_density_pts_channel_width.csv')
+
+
+
+
+
+
