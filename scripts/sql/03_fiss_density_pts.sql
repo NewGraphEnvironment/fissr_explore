1b894b00295c8f924194875f09243e5a81e6339e
Simon Norris <snorris@hillcrestgeo.ca>
2022-01-18 11:23:15 -0800

    latest density pts dump

--- scripts/sql/03_fiss_density_pts.sql
+++ scripts/sql/03_fiss_density_pts.sql
@@ -164,4 +164,5 @@ ON a.linear_feature_id = cw.linear_feature_id
 LEFT OUTER JOIN whse_basemapping.fwa_streams_watersheds_lut l
 ON s.linear_feature_id = l.linear_feature_id
 INNER JOIN whse_basemapping.fwa_watersheds_upstream_area ua
-ON l.watershed_feature_id = ua.watershed_feature_id;
+ON l.watershed_feature_id = ua.watershed_feature_id
+order by a.fiss_density_distinct_id;
