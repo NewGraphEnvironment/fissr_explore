d0050e65a6b4482fa87c992e5c7294501f7235d5
NewGraphEnvironment <al@newgraphenvironment.com>
2022-01-19 06:23:01 -0800

    download the geojson, read in, unnest and burn to csv without the geom info

--- R/0150-download-wshds.R
+++ R/0150-download-wshds.R
@@ -1,6 +1,20 @@
-##download the geojson, read in and burn to csv
+##download the geojson, read in and burn to csv without the geom info
+
+
 url <- 'https://www.hillcrestgeo.ca/outgoing/public/fiss_density_watersheds.geojson'
 destfile <- 'data/fiss_density_watersheds.geojson'
 download.file(url, destfile, quiet = FALSE, mode = "wb")
 
 wshds <- sf::read_sf('https://www.hillcrestgeo.ca/outgoing/public/fiss_density_watersheds.geojson')
+
+wshds_df <- wshds %>%
+  sf::st_drop_geometry() %>%
+  unnest(fiss_density_distinct_ids)
+
+
+##burn the df as a csv to make easy connections later on.
+wshds_df %>%
+  readr::write_csv(file = 'data/fiss_density_wshds_keys.csv')
+
+
+
