94bd0c5a0303905a4aa80f1e014ca48db671e522
Simon Norris <snorris@hillcrestgeo.ca>
2022-01-25 17:55:03 -0800

    include source ids in output watersheds, write elevation data to watershed geojson

--- scripts/sql/05_fiss_density_watersheds.sql
+++ scripts/sql/05_fiss_density_watersheds.sql
@@ -8,14 +8,14 @@ DROP TABLE IF EXISTS temp.fiss_density_watersheds;
 CREATE TABLE temp.fiss_density_watersheds
 (
   fiss_density_watersheds_id serial primary key,
-  fiss_density_distinct_ids integer[],
+  fiss_density_ids integer[],
   geom geometry(MultiPolygon, 3005)
 );
 
 
 INSERT INTO temp.fiss_density_watersheds
 (
-  fiss_density_distinct_ids,
+  fiss_density_ids,
   geom
 )
 
@@ -39,26 +39,32 @@ WITH order8rivs AS
 pts AS
 (
   SELECT
-    array_agg(fiss_density_distinct_id) as fiss_density_distinct_ids,
-    p.wscode_ltree,
-    p.localcode_ltree
-  FROM temp.fiss_density_pts p
--- join to wscodes of streams of order > 8
-  LEFT OUTER JOIN order8rivs b
-  ON p.wscode_ltree = b.wscode_ltree
-  AND p.localcode_ltree < b.localcode_ltree
--- do not include segments with watershed code of order > 8
-  WHERE b.wscode_ltree IS NULL
-  GROUP BY p.wscode_ltree, p.localcode_ltree
+    array_agg(fiss_density_id) as fiss_density_ids,
+    wscode_ltree,
+    localcode_ltree
+  FROM (
+    SELECT
+      unnest(fiss_density_ids) as fiss_density_id,
+      p.wscode_ltree,
+      p.localcode_ltree
+    FROM temp.fiss_density_pts p
+  -- join to wscodes of streams of order > 8
+    LEFT OUTER JOIN order8rivs b
+    ON p.wscode_ltree = b.wscode_ltree
+    AND p.localcode_ltree < b.localcode_ltree
+  -- do not include segments with watershed code of order > 8
+    WHERE b.wscode_ltree IS NULL
+  ) as f
+  GROUP BY wscode_ltree, localcode_ltree
 )
 
 SELECT
-  a.fiss_density_distinct_ids,
+  a.fiss_density_ids,
   ST_Multi(ST_Union(b.geom))::geometry(MultiPolygon, 3005) as geom
 FROM pts a
 INNER JOIN whse_basemapping.fwa_watersheds_poly b 
 ON FWA_Upstream(a.wscode_ltree, a.localcode_ltree, b.wscode_ltree, b.localcode_ltree)
-GROUP BY a.fiss_density_distinct_ids;
+GROUP BY a.fiss_density_ids;
 
 CREATE INDEX ON temp.fiss_density_watersheds USING GIST (geom);
 
